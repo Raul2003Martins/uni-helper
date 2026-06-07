8ff353edebd5f61cd8072e0da987aafb3b7e0f66
Raul Silva Martins <raulmartins128@gmail.com>
2026-06-07 16:09:38 -0300

    muita coisa sendo feita

---
+++ app/src/main/java/br/edu/fatecguarulhos/unihelper/DAOs/MateriaDAO.java
@@ -0,0 +1,42 @@
+package br.edu.fatecguarulhos.unihelper.DAOs;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import br.edu.fatecguarulhos.unihelper.Models.Materia;
+import br.edu.fatecguarulhos.unihelper.Models.Usuario;
+
+public class MateriaDAO {
+
+    private CollectionReference materiaColletion;
+    private FirebaseAuth auth;
+    private Context context;
+
+    public MateriaDAO(Context context){
+        materiaColletion = FirebaseFirestore.getInstance().collection("materia");
+        auth = FirebaseAuth.getInstance();
+        this.context = context;
+    }
+
+    public void cadastrarMateria(Materia materia){
+        registrarMateriaFirebaseAuth(materia);
+        salvarMateriaFirestore(materia);
+    }
+
+    private void registrarMateriaFirebaseAuth(Materia materia){
+
+    }
+    private void salvarMateriaFirestore(Materia materia){
+        materiaColletion.add(materia)
+                .addOnSuccessListener(documentReference -> {
+                }).addOnFailureListener( e ->{});
+    }
+}
